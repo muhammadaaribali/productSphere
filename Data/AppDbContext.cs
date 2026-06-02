97efd4f4b43ade679ad14b1d4b2ed400c30e15ae
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-06-02 22:05:45 +0500

    db connection and users model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore; //import the Entity Framework Core library
+
+using sp1.Models; //import the User model from the Models folder
+
+namespace sp1.Data;
+
+public class AppDbContext : DbContext //create a new class called AppDbContext that inherits from DbContext
+
+{
+    public AppDbContext
+    
+    (DbContextOptions<AppDbContext> options) : base(options) { } 
+    
+    //create a constructor that takes in DbContextOptions and passes it to the base class constructor
+
+    public DbSet<User> Users { get; set; } 
+    
+    //create a DbSet property for the User model, which will allow us to interact with the Users table in the database
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using sp1.Data;//import the AppDbContext class from the Data folder
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+//create a new WebApplicationBuilder instance, which will be used to configure the application
+
+builder.Services.AddControllers(); 
+//add support for controllers, which will handle incoming HTTP requests and return responses
+
 builder.Services.AddEndpointsApiExplorer();
+//add support for API endpoints, which will allow us to define routes for our controllers and generate API documentation
 builder.Services.AddSwaggerGen();
+//add support for Swagger, which will generate API documentation and provide a user interface for testing our API endpoints
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        //get the connection string from the appsettings.json file and use it to configure the DbContext to use PostgreSQL
+    ));
 
 var app = builder.Build();
+//build the application using the configured services and middleware
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //if the application is running in development mode, enable Swagger and the Swagger UI for testing the API endpoints
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
+//map the controllers to the application's request pipeline, which will allow them to handle incoming HTTP requests
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
