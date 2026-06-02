b7df741f73a17f1ddd123dfc2c2730b560a299c4
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-06-02 22:04:09 +0500

    db connection and user model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace sp1.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public int CompanyId { get; set; }
+}
