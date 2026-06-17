a9e814d213b5161e7a24fc585018f852ce135cc9
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-06-18 00:42:23 +0500

    done with login ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,72 @@
+import { useState } from "react";
+import api from "../services/api";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post("/Auth/login", {
+        email,
+        password
+      });
+
+      console.log(response.data);
+
+      alert("Login successful");
+    }
+    catch (error) {
+      console.error(error);
+
+      alert("Login failed");
+    }
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
 
 export default Login;
+
+//UserState 
+/*
+Step 1: Page loads
+email = ""
+
+Input is empty.
+
+Step 2: User types "a"
+setEmail("a")
+
+React updates state:
+
+email = "a"
+
+UI updates automatically.
+ */
