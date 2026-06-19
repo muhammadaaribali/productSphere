106f474fbe05cdc8ab16fdbab83920b474f30c5d
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-06-19 17:53:05 +0500

    done w products page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,9 +1,62 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Products() {
-  return (
-    <div>
-      <h1>Products Page</h1>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            const response = await api.get("/Products");
+            setProducts(response.data);
+        }
+
+        catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Products</h1>
+            {products.map(product => (
+                <div key={product.id}>
+                    <h3>{product.name}</h3>
+                    <p>{product.description}    
+                    </p>
+                    <p>Price: {product.price}   </p>
+                    <hr />
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default Products;
+/*
+User clicks Login
+        ↓
+handleLogin()
+        ↓
+navigate("/products")
+        ↓
+URL becomes /products
+        ↓
+Route matches
+        ↓
+<Products />
+        ↓
+React calls Products()
+        ↓
+useEffect()
+        ↓
+fetchProducts()
+        ↓
+GET /api/Products
+        ↓
+setProducts()
+        ↓
+UI appears
+*/
