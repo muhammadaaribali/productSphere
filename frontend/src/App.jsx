2b4646061978146178bc8849da16446b8fc57f9d
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-07-09 15:35:18 +0500

    signup compoment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Products from "./pages/Products";
 import CreateProduct from "./pages/CreateProduct";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/products" element={<Products />} />
         <Route path="/create-product" element={<CreateProduct />} />
       </Routes>
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
+function Register() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [companyId, setCompanyId] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleRegister = async () => {
+
+        try {
+            await api.post("/Auth/register", {
+                name,
+                email,
+                password,
+                companyId
+            });
+
+            alert("User registered successfully!");
+            navigate("/");
+        }
+        catch (error) {
+
+            console.error(error);
+            alert("Error registering user. Please try again.");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+
+            <br /><br />
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            
+            <br /><br />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br /><br />
+            <input
+                type="number"
+                placeholder="Company ID"
+                value={companyId}
+                onChange={(e) => setCompanyId(e.target.value)}
+            />
+            <br /><br />
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    )
+    
+}
