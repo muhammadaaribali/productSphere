2ac1bde3c983a324112c2bd707478c7a0ffaf092
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-07-15 21:59:35 +0500

    upload to aws structure

--- Services/S3Services.cs
+++ Services/S3Services.cs
@@ -1,5 +1,7 @@
 using Amazon;
 using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 //import the IConfiguration interface from the Microsoft.Extensions.Configuration namespace, which provides access to configuration settings in the application
 
@@ -11,18 +13,38 @@ namespace sp1.Services
         private readonly AmazonS3Client _s3Client;
         private readonly string _bucketName;
 
+        private readonly string _region;
         public S3Service(IConfiguration configuration)
         {
             var accessKey= configuration["AWS:AccessKey"];
             var secretKey= configuration["AWS:SecretKey"];
-            var region= configuration["AWS:Region"];
+            _region= configuration["AWS:Region"]?? throw new Exception("region missing");
 
             _bucketName= configuration["AWS:BucketName"]?? throw new Exception("Bucket name is not configured in appsettings.json");
 
-            var regionEndpoint= RegionEndpoint.GetBySystemName(region);
+            var regionEndpoint= RegionEndpoint.GetBySystemName(_region);
             //region is only a plain text the pc knows it is just a text but the pc does not know what it is so we need to convert it into a region endpoint object using the GetBySystemName method of the RegionEndpoint class to get the info about the region
 
             _s3Client= new AmazonS3Client(accessKey, secretKey, regionEndpoint);
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var fileName= $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+            var request= new PutObjectRequest
+            {
+                BucketName= _bucketName,
+                Key= fileName,
+                InputStream= file.OpenReadStream(),
+                //openreadstream is used to read the contents of the uploaded file as a stream, which can then be sent to S3 for storage
+                ContentType= file.ContentType,
+                //contenttype is used to set the MIME type of the uploaded file, which helps S3 to correctly identify and handle the file
+            };
+
+            await _s3Client.PutObjectAsync(request);
+
+            return $"https://{_bucketName}.s3.{_region}.amazonaws.com/{fileName}";
         }
     }
 }
