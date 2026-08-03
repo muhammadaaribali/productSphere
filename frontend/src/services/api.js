7b0d88d06c461430f82557f7e077ae89a5d9adbb
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-08-03 17:54:44 +0500

    done w response interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -24,7 +24,62 @@ api.interceptors.request.use(
     }
 );
 
+ap1.interceptors.response.use(
+
+  (response) => {
+    return response;
+  },
+
+  async (error) => {
+
+    const originalRequest = error.config;
+    //error.config is the configuration of the request that caused the error. It contains information such as the URL, method, headers, and data of the request.
+
+    if( error.response?.status === 401 && !originalRequest._retry) {
+
+      originalRequest._retry = true;
+
+      const refreshToken= localStorage.getItem("refreshToken");
+
+      const response = await api.post("/Auth/refresh", {
+        refreshToken
+      });
+
+      localStorage.setItem("accessToken",response.data.accessToken);
+
+      localStorage.setItem("refreshToken",response.data.refreshToken);
+
+      originalRequest.headers.Authorization =
+      `Bearer ${response.data.accessToken}`;
+
+      return api(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
+
+
+//internaly entire object is passed as error
+// {
+//     response: {
+//         status: 401,
+//         data: {
+//             message: "Unauthorized"
+//         }
+//     },
+
+//     config: {
+//         method: "GET",
+//         url: "/products",
+//         headers: {
+//             Authorization: "Bearer expiredToken"
+//         }
+//     }
+// }
+
 
 // Notice that request.use() accepts two functions:
 
