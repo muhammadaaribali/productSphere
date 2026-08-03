1add47e96b03663073ef07ece3db10f5e3b75183
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-08-03 16:47:17 +0500

    axios interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,11 +8,15 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(
+
   (config) => {
+
     const accessToken = localStorage.getItem("accessToken");
+
     if (accessToken){
       config.headers.Authorization= `Bearer ${accessToken}`;
     }
+
     return config;
   },
     (error) => {
@@ -21,6 +25,22 @@ api.interceptors.request.use(
 );
 
 export default api;
+
+// Notice that request.use() accepts two functions:
+
+// request.use(
+//     successFunction,
+//     errorFunction
+// )
+
+//this is config
+// {
+//     method: "GET",
+//     url: "/products",
+//     headers: {
+//         Authorization: "Bearer abc123"
+//     }
+// }
 
 //we create an instance of axios with a base URL, so we can easily make API calls to our backend without having to specify the full URL every time. 
 //"My backend is at http://localhost:5067/api"
